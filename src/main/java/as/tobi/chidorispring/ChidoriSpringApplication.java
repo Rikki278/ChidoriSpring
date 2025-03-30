8c13f03ed24a9557c30552531663bc0b1c098786
sviriel <jacobfryu@gmail.com>
2025-03-30 15:00:39 +0300

    Initial commit

---
+++ src/main/java/as/tobi/chidorispring/ChidoriSpringApplication.java
@@ -0,0 +1,13 @@
+package as.tobi.chidorispring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChidoriSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChidoriSpringApplication.class, args);
+    }
+
+}
