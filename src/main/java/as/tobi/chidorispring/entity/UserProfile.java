255993a328b90e33362f0298275926966b9fe496
sviriel <jacobfryu@gmail.com>
2025-04-05 14:57:29 +0300

    #1 added jwt auth

---
+++ src/main/java/as/tobi/chidorispring/entity/UserProfile.java
@@ -0,0 +1,45 @@
+package as.tobi.chidorispring.entity;
+
+import as.tobi.chidorispring.enums.UserRole;
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "user_profile")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserProfile {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserRole role = UserRole.ROLE_USER;
+
+    private String firstName;
+    private String lastName;
+    private String avatarUrl;
+
+    @Column(name = "last_login")
+    private LocalDateTime lastLogin;
+
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt = LocalDateTime.now();
+
+    // Для JWT
+    @Column(name = "refresh_token")
+    private String refreshToken;
+}
---
+++ src/main/java/as/tobi/chidorispring/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package as.tobi.chidorispring.repository;
+
+import as.tobi.chidorispring.entity.UserProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<UserProfile, Long> {
+    Optional<UserProfile> findByEmail(String email);
+}
